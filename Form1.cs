eeaf0dfd563a6918be8afbf20bd96f60b5c1428e
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-10-11 10:53:14 -0700

    agregado BTE

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ namespace ivtxtprocessor;
 public partial class Form1 : Form
 {
     private DataTable dataTable;
-    private string[] exclude = ["PZA", "ENV", "EQP", "AMP", "CJA", "JGO", "LTA"];
+    private string[] exclude = ["PZA", "ENV", "EQP", "AMP", "CJA", "JGO", "LTA", "BTE"];
 
     public Form1()
     {
